5b853867e2b3b061a71f10fbc031cd0ed67f1f62
GabrielASF2 <gabriel_asf2@hotmail.com>
2022-11-04 14:02:25 -0300

    Server, Web & Mobile 1

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,10 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: {props.count}</h1>
   )
 }
 
@@ -8,9 +12,10 @@ export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count')
   const data = await response.json()
 
-  console.log(data)
 
   return {
-    props: {}
+    props: {
+      count: data.count,
+    }
   }
 }
